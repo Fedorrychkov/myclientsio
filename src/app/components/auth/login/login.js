9cdf7f3bbdc19c3e3e4e241ed31e087d2803d012
Fedorrychkov <fedorrychkov@yandex.ru>
2018-01-27 01:18:52 +0300

    Add forms to login and signup

--- src/app/components/auth/login/login.js
+++ src/app/components/auth/login/login.js
@@ -1,11 +1,52 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Formsy from 'formsy-react';
+import FormControl from '../../../shared/ui/form-control/form-control';
 
 class LogIn extends Component {
+    constructor(props) {
+        super(props);
+        this.disableButton = this.disableButton.bind(this);
+        this.enableButton = this.enableButton.bind(this);
+        this.state = { canSubmit: false };
+    }
+
+    disableButton() {
+        this.setState({ canSubmit: false });
+    }
+     
+      enableButton() {
+        this.setState({ canSubmit: true });
+    }
+     
+      submit(model) {
+        // fetch('http://example.com/', {
+        //   method: 'post',
+        //   body: JSON.stringify(model)
+        // });
+        console.log(model);
+    }
+
     render() {
         return (
             <div>
-                isLogin
+                <div className="auth__form">
+                    <Formsy onValidSubmit={this.submit} onValid={this.enableButton} onInvalid={this.disableButton}>
+                        <FormControl
+                            name="username"
+                            label="Логин или E-mail"
+                            validationError=" "
+                            required
+                        />
+                        <FormControl
+                            name="password"
+                            label="Пароль*"
+                            validationError=" "
+                            required
+                        />
+                        <button className="button button__form" type="submit" disabled={!this.state.canSubmit}>Войти</button>
+                    </Formsy>
+                </div>
                 <p>У вас ещё нет аккаунта? <Link to='/auth/signup' className="link link__primary">Зарегистрироваться</Link></p>
             </div>
         );
--- src/app/components/auth/signup/signup.js
+++ src/app/components/auth/signup/signup.js
@@ -1,10 +1,63 @@
 import React, { Component } from 'react';
+import Formsy from 'formsy-react';
+import FormControl from '../../../shared/ui/form-control/form-control';
 
 class SignUp extends Component {
+    constructor(props) {
+        super(props);
+        this.disableButton = this.disableButton.bind(this);
+        this.enableButton = this.enableButton.bind(this);
+        this.state = { canSubmit: false };
+    }
+
+    disableButton() {
+        this.setState({ canSubmit: false });
+    }
+     
+      enableButton() {
+        this.setState({ canSubmit: true });
+    }
+     
+      submit(model) {
+        // fetch('http://example.com/', {
+        //   method: 'post',
+        //   body: JSON.stringify(model)
+        // });
+        console.log(model);
+    }
+
     render() {
         return (
             <div>
-                isSignup
+                <div className="auth__form">
+                    <Formsy onValidSubmit={this.submit} onValid={this.enableButton} onInvalid={this.disableButton}>
+                        <FormControl
+                            name="username"
+                            label="Логин*"
+                            validationError=" "
+                            required
+                        />
+                        <FormControl
+                            name="name"
+                            label="Имя и Фамилия"
+                            validationError=" "
+                        />
+                        <FormControl
+                            name="email"
+                            label="E-mail*"
+                            validations="isEmail"
+                            validationError="Не верный формат E-mail"
+                            required
+                        />
+                        <FormControl
+                            name="password"
+                            label="Пароль*"
+                            validationError=" "
+                            required
+                        />
+                        <button className="button button__form" type="submit" disabled={!this.state.canSubmit}>Зарегистрироваться</button>
+                    </Formsy>
+                </div>
             </div>
         );
     }
